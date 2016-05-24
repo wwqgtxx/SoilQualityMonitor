9c1737e07195b3448da224f41c6bc740de506a6e
wwqgtxx <wwqgtxx@gmail.com>
2016-05-24 18:37:59 +0800

    add serverMode for SensorConnector and a SensorConnectorTester

--- src/io/github/wwqgtxx/soilqualitymonitor/SensorConnector.java
+++ src/io/github/wwqgtxx/soilqualitymonitor/SensorConnector.java
@@ -2,6 +2,7 @@ package io.github.wwqgtxx.soilqualitymonitor;
 
 import java.io.*;
 import java.net.InetAddress;
+import java.net.ServerSocket;
 import java.net.Socket;
 import java.net.UnknownHostException;
 import java.util.concurrent.ArrayBlockingQueue;
@@ -13,18 +14,40 @@ import java.util.concurrent.locks.ReentrantLock;
  */
 public class SensorConnector {
     private SensorConnector(){}
-    private static SensorConnector sensorConnector;
-    private ArrayBlockingQueue<String> queue;
+    private static SensorConnector sensorConnector = new SensorConnector();
+    private ArrayBlockingQueue<String> inputQueue;
+    private ArrayBlockingQueue<String> outputQueue;
     private ReentrantLock lock;
     private Thread connectThread;
     private boolean isInit = false;
     public static SensorConnector getSensorConnector(){
         return sensorConnector;
     }
-    public void init(String host,int port,long timeout,TimeUnit unit){
-        queue = new ArrayBlockingQueue<>(1);
+    public void initClientMode(String host,int port){
+        initClientMode(host,port,2);
+    }
+    public void initClientMode(String host,int port,long timeout){
+        initClientMode(host,port,timeout,TimeUnit.MINUTES);
+    }
+    public void initClientMode(String host,int port,long timeout,TimeUnit unit){
+        inputQueue = new ArrayBlockingQueue<>(1);
+        outputQueue = new ArrayBlockingQueue<>(1);
         lock = new ReentrantLock();
-        connectThread = new Thread(new ConnectThread(host, port, timeout, unit),"SensorConnector.ConnectThread");
+        connectThread = new Thread(new ClientModeConnectThread(host, port, timeout, unit, inputQueue,outputQueue),"SensorConnector.ConnectThread");
+        connectThread.start();
+        isInit = true;
+    }
+    public void initServerMode(int port){
+        initServerMode(port, 2);
+    }
+    public void initServerMode(int port,long timeout){
+        initServerMode(port, timeout,TimeUnit.MINUTES);
+    }
+    public void initServerMode(int port,long timeout,TimeUnit unit){
+        inputQueue = new ArrayBlockingQueue<>(1);
+        outputQueue = new ArrayBlockingQueue<>(1);
+        lock = new ReentrantLock();
+        connectThread = new Thread(new ServerModeConnectThread(port, timeout, unit, inputQueue,outputQueue),"SensorConnector.ConnectThread");
         connectThread.start();
         isInit = true;
     }
@@ -37,8 +60,8 @@ public class SensorConnector {
             lock.lock();
             String rusult = "";
             try {
-                queue.put(input);
-                rusult = queue.take();
+                inputQueue.put(input);
+                rusult = outputQueue.take();
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }finally {
@@ -46,19 +69,47 @@ public class SensorConnector {
             }
             return rusult;
         }
-        return "";
+        else{
+            throw new ConnectorNotInitException();
+        }
+
     }
 
-    public class ConnectThread implements Runnable{
-        private String host;
-        private int port;
+    private abstract class ConnectThread implements Runnable{
+        private ArrayBlockingQueue<String> inputQueue;
+        private ArrayBlockingQueue<String> outputQueue;
         private long timeout;
         private TimeUnit unit;
-        public ConnectThread(String host,int port,long timeout,TimeUnit unit){
-            this.host = host;
-            this.port = port;
+        public ConnectThread(long timeout,TimeUnit unit, ArrayBlockingQueue<String> inputQueue, ArrayBlockingQueue<String> outputQueue){
             this.timeout = timeout;
             this.unit = unit;
+            this.inputQueue = inputQueue;
+            this.outputQueue = outputQueue;
+        }
+        private void parseSocket(Socket socket) throws IOException, InterruptedException {
+            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            PrintWriter out = new PrintWriter(new BufferedWriter(new OutputStreamWriter(socket.getOutputStream())), true);
+            while (true){
+                String input = inputQueue.poll(timeout,unit);
+                if(input!=null) {
+                    out.println(input);
+                    String str = in.readLine();
+                    outputQueue.put(str);
+                }
+                else{
+                    out.println();
+                }
+            }
+        }
+    }
+
+    private class ClientModeConnectThread extends ConnectThread implements Runnable{
+        private String host;
+        private int port;
+        public ClientModeConnectThread(String host,int port,long timeout,TimeUnit unit, ArrayBlockingQueue<String> inputQueue, ArrayBlockingQueue<String> outputQueuee){
+            super(timeout, unit, inputQueue,outputQueuee);
+            this.host = host;
+            this.port = port;
         }
 
         @Override
@@ -66,26 +117,12 @@ public class SensorConnector {
             boolean isInterrupted=false;
             InetAddress addr;
             Socket socket = null;
-            BufferedReader in;
-            PrintWriter out;
+
             while (!isInterrupted){
                 try {
                     addr = InetAddress.getByName(host);
                     socket = new Socket(addr, port);
-                    in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-                    out = new PrintWriter(new BufferedWriter(new OutputStreamWriter(socket.getOutputStream())), true);
-                    while (true){
-                        String input = queue.poll(timeout,unit);
-                        if(input!=null) {
-                            out.println(queue.take());
-                            String str = in.readLine();
-                            queue.put(str);
-                        }
-                        else{
-                            out.println();
-                        }
-                    }
-
+                    super.parseSocket(socket);
                 } catch (UnknownHostException e) {
                     e.printStackTrace();
                 } catch (IOException e) {
@@ -104,4 +141,42 @@ public class SensorConnector {
 
         }
     }
+    private class ServerModeConnectThread extends ConnectThread implements Runnable{
+        private int port;
+        public ServerModeConnectThread(int port,long timeout,TimeUnit unit, ArrayBlockingQueue<String> inputQueue, ArrayBlockingQueue<String> outputQueue){
+            super(timeout, unit, inputQueue,outputQueue);
+            this.port = port;
+        }
+
+        @Override
+        public void run() {
+            boolean isInterrupted=false;
+            ServerSocket serverSocket = null;
+            Socket socket = null;
+            BufferedReader in;
+            PrintWriter out;
+            while (!isInterrupted){
+                try {
+                    serverSocket = new ServerSocket(port);
+                    socket = serverSocket.accept();
+                    super.parseSocket(socket);
+                } catch (UnknownHostException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }finally
+                {
+                    try {
+                        socket.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+
+        }
+    }
+    public class ConnectorNotInitException extends  RuntimeException{}
 }
---
+++ src/io/github/wwqgtxx/soilqualitymonitor/test/SensorConnectorTester.java
@@ -0,0 +1,46 @@
+package io.github.wwqgtxx.soilqualitymonitor.test;
+
+import io.github.wwqgtxx.soilqualitymonitor.SensorConnector;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+
+/**
+ * Created by Administrator on 2016/5/24.
+ */
+public class SensorConnectorTester {
+    public static void main(String args[]) throws Exception{
+        BufferedReader stdin = new BufferedReader(new InputStreamReader(System.in));
+        PrintStream stdout = System.out;
+        SensorConnector sensorConnector = SensorConnector.getSensorConnector();
+        stdout.println("input mode(server or client)");
+
+        String mode = stdin.readLine();
+        switch (mode){
+            case "server":{
+                stdout.println("input port");
+                int port = Integer.parseInt(stdin.readLine());
+                sensorConnector.initServerMode(port);
+                break;
+            }
+            case "client":{
+                stdout.println("input host");
+                String host = stdin.readLine();
+                stdout.println("input port");
+                int port = Integer.parseInt(stdin.readLine());
+                sensorConnector.initClientMode(host,port);
+                break;
+            }
+            default:{
+                stdout.println("error type");
+                main(args);
+            }
+        }
+        stdout.println("now please input string");
+        while (true){
+            stdout.println(sensorConnector.command(stdin.readLine()));
+        }
+
+    }
+}
